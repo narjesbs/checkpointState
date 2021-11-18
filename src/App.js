df49f48671c4f817d306d7c28a295c4136d45344
unknown <narjesbessais8@gmail.com>
2021-11-18 09:55:08 +0100

    updatng image

--- src/App.js
+++ src/App.js
@@ -7,23 +7,22 @@ class App extends React.Component{
   
   constructor(){
     super()
-    this.state = {Person:[{fullName: "Elle Woods",bio: "Gemini vegetarian",profession: "Lawyer", imgSrc: {elle},show: false}]}
+    this.state = {Person:{fullName: "Elle Woods",bio: "Gemini vegetarian",profession: "Lawyer", imgSrc: {elle},show: false}}
   }
 
   shows=()=>{this.setState({ show: !this.state.show })}
  
-  componentDidMount(){this.timerID = setInterval(() => this.time(),1000);}
-  
-  time(){this.setState({date: new Date()});}
-  
   render(){
+    console.log()
     return (
       <div>
         <button style={{border: "none",outline: "0",display: "inline-block",padding: "8px",color: "white",
         backgroundColor: "#000",textAlign: "center",cursor: "pointer",width: "100%",fontSize: "18px"}} 
         onClick={this.shows} > { this.state.show ? "hide" :"show" } </button>
         {this.state.show  ?  
-          <Profile data = {this.Person}/> 
+        <div>
+          <img src = {elle} alt="this is elle " style={{width:"100%"}} />
+          <Profile data = {this.state.Person}/> </div>
           : null
           }
       </div>
--- src/profile.js
+++ src/profile.js
@@ -12,13 +12,13 @@ class Profile extends React.Component{
   time(){this.setState({date: new Date()});}
   
   render(){
+    console.log(this.props)
     return (
         <div className = "card">
-            <img src = {this.props.elle} alt="this is elle "/>
-            <h4 style ={{color: "pink",fontSize: "15px"}}>I'm a {this.props.fullName},</h4>
-            <h4 style ={{color: "pink",fontSize: "15px"}}>I'm a {this.props.bio},</h4>
-            <h4 style ={{color: "pink",fontSize: "15px"}}>and I am a {this.props.profession}</h4>
-            <h5>It is {this.state.date.toLocaleTimeString()}.</h5>
+            <h4 style ={{color: "pink",fontSize: "15px"}}>I'm a {this.props.data.fullName},</h4>
+            <h4 style ={{color: "pink",fontSize: "15px"}}>I'm a {this.props.data.bio},</h4>
+            <h4 style ={{color: "pink",fontSize: "15px"}}>and I am a {this.props.data.profession}</h4>
+            <h5>It is {this.state.date.toLocaleTimeString()}.</h5> 
         </div>  
     )
   }  
